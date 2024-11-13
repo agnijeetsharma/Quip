e1491a6d83d6209b22f73fbf51540e15875b785b
name <123867179+agnijeetsharma@users.noreply.github.com>
2024-11-13 21:55:56 +0530

    created validation using zod for user schema

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -3,7 +3,7 @@ import {z} from 'zod'
 export const usernameValidation=z.string().min(2,"username must be two char").
 max(30,"username must be less than 30 char").
 regex(/^[a-zA-Z0-9._-]{3,16}$/,'username must not conatain special char')
-export const userSignUpValidation=z.object({
+export const userSignUpSchema=z.object({
          username:usernameValidation,
          email:z.string().email({message:"invalid email"}),
          password:z.string().min(8,{message:"password must be 8 char"})
